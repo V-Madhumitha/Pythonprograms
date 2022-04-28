2de2fd875b279d2f31578bcbd92f9e4b26e4f6eb
V-Madhumitha <102716133+V-Madhumitha@users.noreply.github.com>
2022-04-28 14:15:09 +0530

    Create multiples.py

---
+++ Functions/multiples.py
@@ -0,0 +1,9 @@
+def num(range):
+  a=1
+  b=25
+  for i in range(a,b+1):
+    if (i%5!=0):
+      print(i)
+num(range)
+m=int(input("Enter the first number:"))
+n=int(input("Enter the second number:"))
